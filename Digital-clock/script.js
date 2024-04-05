10b3a7b2ed8fd352d45cb47a9a4ad98e9d12b33c
N1kunj1998 <njkhakhkhar@gmail.com>
2024-04-06 00:09:09 +0530

    Completed Digital Clock Project

---
+++ Digital-clock/script.js
@@ -0,0 +1,24 @@
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const seconds = document.getElementById("seconds");
+const ampm = document.getElementById("ampm");
+
+//Add 0 to the begining of number if less than 10
+function formatTime(time) {
+    return time.toString().padStart(2, "0");
+}
+
+function clock() {
+    const date = new Date();
+
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    hour.textContent = formatTime(h);
+    minute.textContent = formatTime(m);
+    seconds.textContent = formatTime(s);
+    ampm.textContent = (h >= 12 ? "PM" : "AM");
+}
+
+setInterval(clock, 1000);
